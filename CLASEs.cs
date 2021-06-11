e0cdd7cedaf2c6a3e556e7d251a676fae8504545
sabrinagalera <sabrinapgalera@gmail.com>
2021-06-11 14:31:29 -0300

    first commit

---
+++ CLASEs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CLASEs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ----------- Ejercicio 1 Calcular  área de  un círculo
+
+            Console.WriteLine("Cálculo áreas \n");
+
+            Circulo miCirculo;
+
+            miCirculo = new Circulo();
+            Console.WriteLine("Círculo 1: " + miCirculo.CalculoArea(8));
+
+            Circulo miCirculo2 = new Circulo();
+            Console.WriteLine("Círculo 2: " + miCirculo2.CalculoArea(10));
+
+            // ---------- Ejercicio 2 Convertir dólar a euro y cambiar la cotización
+
+            Console.WriteLine("\nConversor dólar a euro \n");
+            ConversorDolarEuro obj = new ConversorDolarEuro();
+            Console.WriteLine("Ayer : " + obj.Convierte(10.0));
+
+            ConversorDolarEuro obj2 = new ConversorDolarEuro();
+            obj2.CambiaValorEuro(1.850);
+            Console.WriteLine("\nHoy: " + obj2.Convierte(10.0));
+
+        }
+    }
+
+    class Circulo
+    {
+        private const double PI = 3.1416;
+        public double CalculoArea(int radio)
+        {
+            return PI * radio * radio;
+        }
+    }
+
+    class ConversorDolarEuro
+    {
+        private double euro = 1.555;
+
+        public double Convierte(double cantidad)
+        {
+            return cantidad * euro;
+        }
+        public double CambiaValorEuro(double nuevoValor)
+        {
+            if (nuevoValor > 0) euro = nuevoValor;
+            return euro;
+        }
+
+    }
+}
---
+++ GETSET.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace GETSET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heladera miHeladera1 = new Heladera();
+            Console.WriteLine("Heladera 1: " + miHeladera1.getInfoHeladera());
+
+            Heladera miHeladera2 = new Heladera("Philco", 0.75);
+            miHeladera2.setInfoHeladera("Beige", 1.80);
+            Console.WriteLine("Heladera 2: " + miHeladera2.getInfoHeladera());
+
+            Heladera miHeladera3 = new Heladera("Gama", 0.80);
+            miHeladera3.setInfoHeladera("Blanca", 2.00);
+            Console.WriteLine("Heladera 3: " + miHeladera3.getInfoHeladera());
+
+
+
+        }
+    }
+
+    partial class Heladera
+    {
+        public Heladera()
+        {
+            marca = "Mabel";
+            grados = -15;
+            profundidad = 0.60;
+            freezer = true;
+            color = "Blanco";
+            alto = 1.90;
+        }
+
+        public Heladera(string marcaHela, double profHela)
+        {
+            marca = marcaHela;
+            grados = -15;
+            profundidad = profHela;
+            freezer = true;
+        }
+    }
+
+    partial class Heladera
+    {
+        public string getInfoHeladera()
+        {
+            return "Info Heladera:\n" + " Marca: " + marca + " \n Freezer: " + freezer + "\n Grados: " + grados + "\n Profundidad: " + profundidad + "\n Alto: " + alto + "\n Color: " + color;
+        }
+
+        public void setInfoHeladera(string color, double alto)
+        {
+            this.color = color;
+            this.alto = alto;
+        }
+
+        string marca;
+        int grados;
+        double profundidad;
+        string color;
+        double alto;
+        bool freezer;
+    }
+}
+
